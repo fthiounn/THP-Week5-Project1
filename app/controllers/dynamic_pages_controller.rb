e967d9c77b93922416d9ff38ee81b286824bef56
fthiounn <fthiounn@gmail.com>
2019-10-28 17:23:06 +0100

    couche css et UI

--- app/controllers/dynamic_pages_controller.rb
+++ app/controllers/dynamic_pages_controller.rb
@@ -8,5 +8,8 @@ class DynamicPagesController < ApplicationController
   end
   def user
     params[:user] = User.find(params[:id])
+    params[:city] = City.find(params[:user].city_id)
+    params[:nb_gossips] = Gossip.where(user_id: params[:user].id).count
+    params[:nb_comments] = Gossip.where(user_id: params[:user].id).count
   end
 end
