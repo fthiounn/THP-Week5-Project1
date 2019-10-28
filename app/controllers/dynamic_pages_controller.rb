fb0497c67dfdaab4822ac89c258b113f8c6033f3
fthiounn <fthiounn@gmail.com>
2019-10-28 16:34:00 +0100

    ajout des pages user et gossip

--- app/controllers/dynamic_pages_controller.rb
+++ app/controllers/dynamic_pages_controller.rb
@@ -2,4 +2,11 @@ class DynamicPagesController < ApplicationController
   def home
     params[:user] = User.find(params[:id]) #POUR LA RECHERCHE EN BDD
   end
+  def gossip   
+    params[:gossip] = Gossip.find(params[:id])
+    params[:comments] = Comment.where(gossip_id: params[:id]).all
+  end
+  def user
+    params[:user] = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'dynamic_pages/home'
+  get 'dynamic_pages/gossip'
+  get 'dynamic_pages/user'
   get '/static_pages/home', to: 'static_pages#home'
   get '/static_pages/team', to: 'static_pages#team'
   get '/static_pages/contact', to: 'static_pages#contact'
-  get '/home/:id', to: 'dynamic_pages#home'
+  get '/home/:id', to: 'dynamic_pages#home', as: 'home'
+  get '/gossip/:id', to: 'dynamic_pages#gossip', as: 'gossip'
+  get '/user/:id', to: 'dynamic_pages#user', as: 'user'
 end
